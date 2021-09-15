4c50ee281c873ec7716a6b37ed793a7ff7c046c8
ClementRayer <clement.rayer@gmail.com>
2021-09-16 00:35:31 +0200

    Team names fully operationals, connected w/ canvas

--- js/script.js
+++ js/script.js
@@ -19,23 +19,19 @@ draw = () => {
 };
 
 // TEMPORARY (to make it iterate with the input IDs) : Puts the different texts in the canvas
-putText = () =>{
+putText = (conference) =>{
     ctx.fillStyle = "#000000";
-    ctx.fillText(seed1.value.toUpperCase(), 55, 170);
-    ctx.fillText(seed2.value.toUpperCase(), 55, 202);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 234);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 266);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 298);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 330);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 362);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 394);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 426);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 458);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 490);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 522);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 554);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 586);
-    ctx.fillText('EQUIPE 2 SKETBA', 55, 618);
+    if(conference == 'west'){
+        for(k = 1; k < 16; k++){
+            let seedToGet = document.getElementById(`input-${conference}-${k}`);
+            ctx.fillText(seedToGet.value, 55, 138+(32 * k))
+        }
+    }else{
+        for(k = 1; k < 16; k++){
+            let seedToGet = document.getElementById(`input-${conference}-${k}`);
+            ctx.fillText(seedToGet.value, 340, 138+(32 * k))
+        }
+    }
     ctx.fillStyle = "#20c91c";
     ctx.fillText('72-10', 263, 169);
 };
@@ -48,7 +44,8 @@ let seed2 = document.getElementById('seed2');
 validationButton.addEventListener('click', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     putBackgroundImage();
-    putText();
+    putText('west');
+    putText('east');
     downloadButton.href = canvas.toDataURL("image/png");
 });
 
@@ -60,6 +57,7 @@ validationButton.addEventListener('click', () => {
 
 // Create variables for input generation
 let westConferenceInputs = document.getElementById('west-conference-inputs');
+let eastConferenceInputs = document.getElementById('east-conference-inputs');
 
 // Creates the inputs for the West Conference > TO OPTIMISE WITH ONLY ONE FUNCTION? WOULD PASS THE ELEMENT EAST OR WEST, THAT DEFINES IN WHICH TEAM POOL WE CHOSE AND USES IDs TO SEPARATE CONFERENCES
 placeWestChoices = () =>{
@@ -74,10 +72,31 @@ placeWestChoices = () =>{
         placeHolder.appendChild(placeHolderText);
         singleInput.appendChild(placeHolder);
         // Places the needed teams in each input
-        for(let i = 0; i < teamsList.length; i++){
+        for(let i = 0; i < westTeamsList.length; i++){
             let aimedInput = document.getElementById(`input-west-${j}`);
             let optionItem = document.createElement('option');
-            let valueTeamName = document.createTextNode(teamsList[i]);
+            let valueTeamName = document.createTextNode(westTeamsList[i]);
+            optionItem.appendChild(valueTeamName);
+            aimedInput.appendChild(optionItem);
+        }
+    }
+};
+placeEastChoices = () =>{
+    // Creates 15 inputs
+    for(let j = 1; j < 16; j++){
+        let singleInput = document.createElement('select');
+        singleInput.setAttribute('id', `input-east-${j}`);
+        eastConferenceInputs.appendChild(singleInput);
+        // Sets a placeholder
+        let placeHolder = document.createElement('option');
+        let placeHolderText = document.createTextNode(`Seed #${j}`);
+        placeHolder.appendChild(placeHolderText);
+        singleInput.appendChild(placeHolder);
+        // Places the needed teams in each input
+        for(let i = 0; i < eastTeamsList.length; i++){
+            let aimedInput = document.getElementById(`input-east-${j}`);
+            let optionItem = document.createElement('option');
+            let valueTeamName = document.createTextNode(eastTeamsList[i]);
             optionItem.appendChild(valueTeamName);
             aimedInput.appendChild(optionItem);
         }
@@ -87,3 +106,4 @@ placeWestChoices = () =>{
 // Trigger on page load : canvas creation, choices in selectors
 window.onload = draw();
 window.onload = placeWestChoices();
+window.onload = placeEastChoices();
