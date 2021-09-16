00c852ceaa6b64ae425d11f0b0dcba46d4b64525
ClementRayer <clement.rayer@gmail.com>
2021-09-16 18:56:11 +0200

    Design tweaks, added reset and logos in lib

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@ let ctx = canvas.getContext('2d');
 ctx.font = '700 14px helvetica';
 let img = document.getElementById('source');
 let validationButton = document.getElementById('validation-button');
+let resetButton = document.getElementById('reset-button');
 let downloadButton = document.getElementById('download-button');
+
+// Create variables for input generation
+let westConferenceInputs = document.getElementById('west-conference-inputs');
+let eastConferenceInputs = document.getElementById('east-conference-inputs');
 
 // Sets the background (called in other functions)
 putBackgroundImage = () =>{
@@ -15,7 +20,6 @@ putBackgroundImage = () =>{
 draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     putBackgroundImage();
-    ctx.fillText('zzccmxtp', 56, 202);
 };
 
 // TEMPORARY (to make it iterate with the input IDs) : Puts the different texts in the canvas
@@ -24,21 +28,21 @@ putText = (conference) =>{
     if(conference == 'west'){
         for(k = 1; k < 16; k++){
             let seedToGet = document.getElementById(`input-${conference}-${k}`);
-            ctx.fillText(seedToGet.value, 55, 138+(32 * k))
+            ctx.fillStyle = "#20c91c";
+            ctx.fillText(`${k}.`, 55, 138+(32 * k));
+            ctx.fillStyle = "#000000";
+            ctx.fillText(seedToGet.value, 75, 138+(32 * k));
         }
     }else{
         for(k = 1; k < 16; k++){
             let seedToGet = document.getElementById(`input-${conference}-${k}`);
-            ctx.fillText(seedToGet.value, 340, 138+(32 * k))
+            ctx.fillStyle = "#20c91c";
+            ctx.fillText(`${k}.`, 340, 138+(32 * k));
+            ctx.fillStyle = "#000000";
+            ctx.fillText(seedToGet.value, 360, 138+(32 * k));
         }
     }
-    ctx.fillStyle = "#20c91c";
-    ctx.fillText('72-10', 263, 169);
 };
-
-// TEMPORARY : Uses the only input yet to test the writing
-let seed1 = document.getElementById('seed1');
-let seed2 = document.getElementById('seed2');
 
 // Resets the canvas, set back image and uses the (temporary) text, prepares the download CTA to download the validated canvas
 validationButton.addEventListener('click', () => {
@@ -49,54 +53,26 @@ validationButton.addEventListener('click', () => {
     downloadButton.href = canvas.toDataURL("image/png");
 });
 
-// TO USE : Updates the canvas when typing, avoid waiting to press the validation CTA
-// const allInputs = document.querySelector('input');
-// allInputs.addEventListener('keyup', () =>{
-//     console.log('porut');
-// });
-
-// Create variables for input generation
-let westConferenceInputs = document.getElementById('west-conference-inputs');
-let eastConferenceInputs = document.getElementById('east-conference-inputs');
-
-// Creates the inputs for the West Conference > TO OPTIMISE WITH ONLY ONE FUNCTION? WOULD PASS THE ELEMENT EAST OR WEST, THAT DEFINES IN WHICH TEAM POOL WE CHOSE AND USES IDs TO SEPARATE CONFERENCES
-placeWestChoices = () =>{
+// Creates the inputs for the Conferences
+placeChoices = (conference) =>{
+    // Set the variables to serve west or east content (DOM element + team pool)
+    let concernedInput = (conference === 'west') ? westConferenceInputs : eastConferenceInputs;
+    let teamPools = (conference === 'west') ? westTeamsList : eastTeamsList;
     // Creates 15 inputs
     for(let j = 1; j < 16; j++){
         let singleInput = document.createElement('select');
-        singleInput.setAttribute('id', `input-west-${j}`);
-        westConferenceInputs.appendChild(singleInput);
+        singleInput.setAttribute('id', `input-${conference}-${j}`);
+        concernedInput.appendChild(singleInput);
         // Sets a placeholder
         let placeHolder = document.createElement('option');
         let placeHolderText = document.createTextNode(`Seed #${j}`);
         placeHolder.appendChild(placeHolderText);
         singleInput.appendChild(placeHolder);
         // Places the needed teams in each input
-        for(let i = 0; i < westTeamsList.length; i++){
-            let aimedInput = document.getElementById(`input-west-${j}`);
+        for(let i = 0; i < teamPools.length; i++){
+            let aimedInput = document.getElementById(`input-${conference}-${j}`);
             let optionItem = document.createElement('option');
-            let valueTeamName = document.createTextNode(westTeamsList[i]);
-            optionItem.appendChild(valueTeamName);
-            aimedInput.appendChild(optionItem);
-        }
-    }
-};
-placeEastChoices = () =>{
-    // Creates 15 inputs
-    for(let j = 1; j < 16; j++){
-        let singleInput = document.createElement('select');
-        singleInput.setAttribute('id', `input-east-${j}`);
-        eastConferenceInputs.appendChild(singleInput);
-        // Sets a placeholder
-        let placeHolder = document.createElement('option');
-        let placeHolderText = document.createTextNode(`Seed #${j}`);
-        placeHolder.appendChild(placeHolderText);
-        singleInput.appendChild(placeHolder);
-        // Places the needed teams in each input
-        for(let i = 0; i < eastTeamsList.length; i++){
-            let aimedInput = document.getElementById(`input-east-${j}`);
-            let optionItem = document.createElement('option');
-            let valueTeamName = document.createTextNode(eastTeamsList[i]);
+            let valueTeamName = document.createTextNode(teamPools[i]);
             optionItem.appendChild(valueTeamName);
             aimedInput.appendChild(optionItem);
         }
@@ -105,5 +81,15 @@ placeEastChoices = () =>{
 
 // Trigger on page load : canvas creation, choices in selectors
 window.onload = draw();
-window.onload = placeWestChoices();
-window.onload = placeEastChoices();
+window.onload = placeChoices('west');
+window.onload = placeChoices('east');
+
+// Reset the choices by regenerating the inputs
+resetButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    putBackgroundImage();
+    westConferenceInputs.innerHTML = '';
+    eastConferenceInputs.innerHTML = '';
+    window.onload = placeChoices('west');
+    window.onload = placeChoices('east');
+});
--- js/teamslist.js
+++ js/teamslist.js
@@ -1,2 +1,34 @@
-let westTeamsList = ['Dallas Mavericks', 'Denver Nuggets', 'Golden State Warriors', 'Houston Rockets', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'Oklahoma City Thunder', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'Utah Jazz', 'San Antonio Spurs']
-let eastTeamsList = ['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Detroit Pistons', 'Indiana Pacers', 'Miami Heat', 'Milwaukee Bucks', 'New York Knicks', 'Orlando Magic', 'Philadelphia 76ers','Toronto Raptors', 'Washington Wizards']
+let westTeamsList = ['Dallas Mavericks', 'Denver Nuggets', 'Golden State Warriors', 'Houston Rockets', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'Oklahoma City Thunder', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'Utah Jazz', 'San Antonio Spurs'];
+let eastTeamsList = ['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Detroit Pistons', 'Indiana Pacers', 'Miami Heat', 'Milwaukee Bucks', 'New York Knicks', 'Orlando Magic', 'Philadelphia 76ers','Toronto Raptors', 'Washington Wizards'];
+// let teamsLogo = [
+//     {TeamName : 'Atlanta Hawks', ImageName : 'atlanta-hawks.svg'},
+//     {TeamName : 'Boston Celtics', ImageName : 'boston-celtics.svg'},
+//     {TeamName : 'Brooklyn Nets', ImageName : 'brooklyn-nets.svg'},
+//     {TeamName : 'Charlotte Hornets', ImageName : 'charlotte-hornets.svg'},
+//     {TeamName : 'Chicago Bulls', ImageName : 'chicago-bulls.svg'},
+//     {TeamName : 'Cleveland Cavaliers', ImageName : 'cleveland-cavaliers.svg'},
+//     {TeamName : 'Dallas Mavericks', ImageName : 'dallas-mavericks.svg'},
+//     {TeamName : 'Denver Nuggets', ImageName : 'denver-nuggets.svg'},
+//     {TeamName : 'Detroit Pistons', ImageName : 'detroit-pistons.svg'},
+//     {TeamName : 'Golden State Warriors', ImageName : 'golden-state-warriors.svg'},
+//     {TeamName : 'Houston Rockets', ImageName : 'houston-rockets.svg'},
+//     {TeamName : 'Indiana Pacers', ImageName : 'indiana-pacers.svg'},
+//     {TeamName : 'Los Angeles Clippers', ImageName : 'los-angeles-clippers.svg'},
+//     {TeamName : 'Los Angeles Lakers', ImageName : 'los-angeles-lakers.svg'},
+//     {TeamName : 'Memphis Grizzlies', ImageName : 'memphis-grizzlies.svg'},
+//     {TeamName : 'Miami Heat', ImageName : 'miami-heat.svg'},
+//     {TeamName : 'Milwaukee Bucks', ImageName : 'milwaukee-bucks.svg'},
+//     {TeamName : 'Minnesota Timberwolves', ImageName : 'minnesota-timberwolves.svg'},
+//     {TeamName : 'New Orleans Pelicans', ImageName : 'new-orleans-pelicans.svg'},
+//     {TeamName : 'New York Knicks', ImageName : 'new-york-knicks.svg'},
+//     {TeamName : 'Oklahoma City Thunder', ImageName : 'oklahoma-city-thunder.svg'},
+//     {TeamName : 'Orlando Magic', ImageName : 'orlando-magic.svg'},
+//     {TeamName : 'Philadelphia 76ers', ImageName : 'philadelphia-76ers.svg'},
+//     {TeamName : 'Phoenix Suns', ImageName : 'phoenix-suns.svg'},
+//     {TeamName : 'Portland Trail Blazers', ImageName : 'portland-trail-blazers.svg'},
+//     {TeamName : 'Sacramento Kings', ImageName : 'sacramento-kings.svg'},
+//     {TeamName : 'San Antonio Spurs', ImageName : 'san-antonio-spurs.svg'},
+//     {TeamName : 'Toronto Raptors', ImageName : 'toronto-raptors.svg'},
+//     {TeamName : 'Utah Jazz', ImageName : 'utah-jazz.svg'},
+//     {TeamName : 'Washington Wizards', ImageName : 'washington-wizards.svg'},
+// ];
