823cfd89cf5f4d0a91ef505a0fb1ffb9ee9eef05
ClementRayer <clement.rayer@gmail.com>
2021-09-20 17:27:35 +0200

    Added Win input

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 // Declare all usable variables for the canvas
-let canvas = document.getElementById('test-canvas');
-let ctx = canvas.getContext('2d');
-ctx.font = '700 14px helvetica';
-let img = document.getElementById('source');
-let validationButton = document.getElementById('validation-button');
-let resetButton = document.getElementById('reset-button');
-let downloadButton = document.getElementById('download-button');
+var canvas = document.getElementById('export-canvas');
+var ctx = canvas.getContext('2d');
+ctx.font = '700 30px helvetica';
+var img = document.getElementById('source');
+var validationButton = document.getElementById('validation-button');
+var resetButton = document.getElementById('reset-button');
+var downloadButton = document.getElementById('download-button');
+
+// Declare variables about the canvas shown in the page
+var shownCanvas = document.getElementById('shown-canvas');
+var shownCtx = shownCanvas.getContext('2d');
 
 // Create variables for input generation
-let westConferenceInputs = document.getElementById('west-conference-inputs');
-let eastConferenceInputs = document.getElementById('east-conference-inputs');
+var westConferenceInputs = document.getElementById('west-conference-inputs');
+var eastConferenceInputs = document.getElementById('east-conference-inputs');
 
 // Sets the background (called in other functions)
 putBackgroundImage = () =>{
-    ctx.drawImage(img, 0, 0, 1280, 1280, 0, 0, 640, 640);
+    ctx.drawImage(img, 0, 0, 1280, 1280, 0, 0, 1280, 1280);
+    updateShownCanvas();
 };
 
 // Create the basic canvas, only a background
@@ -22,29 +27,48 @@ draw = () => {
     putBackgroundImage();
 };
 
-// TEMPORARY (to make it iterate with the input IDs) : Puts the different texts in the canvas
+// Puts the input texts in the canvas, shown canvas copies the export one
 putText = (conference) =>{
-    ctx.fillStyle = "#000000";
-    if(conference == 'west'){
-        for(k = 1; k < 16; k++){
-            let seedToGet = document.getElementById(`input-${conference}-${k}`);
-            ctx.fillStyle = "#20c91c";
-            ctx.fillText(`${k}.`, 55, 138+(32 * k));
-            ctx.fillStyle = "#000000";
-            ctx.fillText(seedToGet.value, 75, 138+(32 * k));
-        }
+    // Adapts positions regarding the current conference
+    if(conference === 'west'){
+        var currentConference = 'west';
+        var rankXReference = 110;
+        var teamXReference = 152;
+        var winLossXReference = 523;
     }else{
-        for(k = 1; k < 16; k++){
-            let seedToGet = document.getElementById(`input-${conference}-${k}`);
-            ctx.fillStyle = "#20c91c";
-            ctx.fillText(`${k}.`, 340, 138+(32 * k));
-            ctx.fillStyle = "#000000";
-            ctx.fillText(seedToGet.value, 360, 138+(32 * k));
-        }
-    }
+        var currentConference = 'east';
+        var rankXReference = 681;
+        var teamXReference = 723;
+        var winLossXReference = 1095;
+    };
+    ctx.fillStyle = "#000000";
+    for(k = 1; k < 16; k++){
+        // Places the team name
+        var seedToGet = document.getElementById(`input-${currentConference}-${k}`);
+        ctx.font = '700 30px helvetica';
+        ctx.fillStyle = "#20c91c";
+        ctx.fillText(`${k}.`, rankXReference, 276+(64 * k));
+        ctx.fillStyle = "#000000";
+        ctx.fillText(seedToGet.value, teamXReference, 276+(64 * k));
+        // Calculates the score based on inputed win number
+        var resultToGet = document.getElementById(`input-result-${currentConference}-${k}`);
+        var winsInputed = resultToGet.value;
+        var defeatsCalculated = 82 - winsInputed;
+        var calculatedWinLosses = `${winsInputed} - ${defeatsCalculated}`
+        // Places the calculated team score
+        ctx.font = '700 26px helvetica';
+        ctx.fillStyle = "#20c91c";
+        ctx.fillText(calculatedWinLosses, winLossXReference, 276+(64 * k));
+    };
+    updateShownCanvas();
 };
 
-// Resets the canvas, set back image and uses the (temporary) text, prepares the download CTA to download the validated canvas
+// Updates the canvas shown on the page
+updateShownCanvas = () =>{
+    shownCtx.drawImage(canvas, 0, 0, 1280, 1280, 0, 0, 1000, 1000);
+}
+
+// Validate choices : resets the canvas, set back image and uses the text, prepares the download CTA to download the validated canvas
 validationButton.addEventListener('click', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     putBackgroundImage();
@@ -56,27 +80,41 @@ validationButton.addEventListener('click', () => {
 // Creates the inputs for the Conferences
 placeChoices = (conference) =>{
     // Set the variables to serve west or east content (DOM element + team pool)
-    let concernedInput = (conference === 'west') ? westConferenceInputs : eastConferenceInputs;
-    let teamPools = (conference === 'west') ? westTeamsList : eastTeamsList;
+    var concernedInput = (conference === 'west') ? westConferenceInputs : eastConferenceInputs;
+    var teamPools = (conference === 'west') ? westTeamsList : eastTeamsList;
     // Creates 15 inputs
-    for(let j = 1; j < 16; j++){
-        let singleInput = document.createElement('select');
+    for(var j = 1; j < 16; j++){
+        // Creates div
+        var inputDiv = document.createElement('div');
+        inputDiv.setAttribute('class', 'input-line');
+        concernedInput.appendChild(inputDiv);
+        // Creates the team picker
+        var singleInput = document.createElement('select');
+        singleInput.setAttribute('class', 'input-team-name');
         singleInput.setAttribute('id', `input-${conference}-${j}`);
-        concernedInput.appendChild(singleInput);
+        inputDiv.appendChild(singleInput);
         // Sets a placeholder
-        let placeHolder = document.createElement('option');
-        let placeHolderText = document.createTextNode(`Seed #${j}`);
+        var placeHolder = document.createElement('option');
+        var placeHolderText = document.createTextNode(`Seed #${j}`);
         placeHolder.appendChild(placeHolderText);
         singleInput.appendChild(placeHolder);
         // Places the needed teams in each input
-        for(let i = 0; i < teamPools.length; i++){
-            let aimedInput = document.getElementById(`input-${conference}-${j}`);
-            let optionItem = document.createElement('option');
-            let valueTeamName = document.createTextNode(teamPools[i]);
+        for(var i = 0; i < teamPools.length; i++){
+            var aimedInput = document.getElementById(`input-${conference}-${j}`);
+            var optionItem = document.createElement('option');
+            var valueTeamName = document.createTextNode(teamPools[i]);
             optionItem.appendChild(valueTeamName);
             aimedInput.appendChild(optionItem);
-        }
-    }
+        };
+        // Creates the score inputs
+        var scoreSingleInput = document.createElement('input');
+        scoreSingleInput.setAttribute('type', 'number');
+        scoreSingleInput.setAttribute('class', 'input-wins')
+        scoreSingleInput.setAttribute('min', '0');
+        scoreSingleInput.setAttribute('max', '82');
+        scoreSingleInput.setAttribute('id', `input-result-${conference}-${j}`);
+        inputDiv.appendChild(scoreSingleInput);
+    };
 };
 
 // Trigger on page load : canvas creation, choices in selectors
@@ -90,6 +128,7 @@ resetButton.addEventListener('click', () => {
     putBackgroundImage();
     westConferenceInputs.innerHTML = '';
     eastConferenceInputs.innerHTML = '';
-    window.onload = placeChoices('west');
-    window.onload = placeChoices('east');
+    placeChoices('west');
+    placeChoices('east');
+    updateShownCanvas();
 });
